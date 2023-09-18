a6f93de452a78fbde346312f92403ba4f3116954
Oleh Karapysh <sebastianvell777@gmail.com>
2023-09-18 22:16:46 +0300

    removed empty line

--- backend/TestTask.WebAPI/Program.cs
+++ backend/TestTask.WebAPI/Program.cs
@@ -3,7 +3,6 @@ using TestTask.WebAPI.Extensions;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
